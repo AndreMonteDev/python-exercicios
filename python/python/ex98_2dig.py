8ae6260e17ec1f003d1978d7701ca17fc9af38bc
Andre Monte <andremonte.dev@gmail.com>
2024-11-27 12:47:04 -0300

    atualizacao de exercicios

--- python/python/ex98_2dig.py
+++ python/python/ex98_2dig.py
@@ -1,6 +1,6 @@
 
 
-cpf_digitado = '22311972866'
+cpf_digitado = '83015692046'
 cpf1 = cpf_digitado[:9]
 i_reg = 10
 resultado_dig1 = 0
@@ -12,12 +12,7 @@ for numero1 in cpf1:
 digito_1 = ((resultado_dig1 * 10) % 11)
 digito_1 = digito_1 if digito_1 <= 9 else 0
 
-print(digito_1)
-
 cpf_dig1 = str(cpf1) + str(digito_1)
-
-print(cpf_dig1)
-
 
 j_reg = 11
 resultado_dig2 = 0
@@ -29,19 +24,16 @@ for numero2 in cpf_dig1:
 digito_2 = ((resultado_dig2 * 10) % 11)
 digito_2 = digito_2 if digito_2 <= 9 else 0
 
-print(digito_2)
 
 cpf_completo = str(cpf_dig1) + str(digito_2)
 
-print(cpf_completo)
-
-print(cpf_completo[9::])
-
 digito_completo = str(digito_1) + str(digito_2)
 
-print(digito_completo)
+print(cpf_digitado)
+print(cpf_completo)
 
-if cpf_digitado[9::] == digito_completo:
+
+if cpf_digitado == cpf_completo:
     print('CPF válido')
 else:
     print('CPF inválido')
