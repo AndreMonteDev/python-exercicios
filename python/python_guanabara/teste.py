f196e3e669346c2071f6ab61210ff49f8cd14525
andre <andre@debian-BULLSEYE-live-builder-AMD64>
2025-03-14 17:03:03 -0300

    atualização de exercicios Guanabara

--- python/python_guanabara/teste.py
+++ python/python_guanabara/teste.py
@@ -1,8 +1,6 @@
-n1 = float(10)
-n2 = float(2)
+import math
 
-soma = n1 + n2
-subtracao = n1 - n2
-multiplica = n1 * n2
-divisao = n1 / n2
-print(f'A soma é {}, A subtracao é {}, a multiplicacao é {} e a divisao é {}'soma, subtracao, multiplica, divisao)
+n = 6.876
+
+print(n)
+print(math.trunc(n))
