3fcdb72a4f1526536e18ae30c845adfd91d8f5fa
AndreMonteDev <andremonte.dev@gmail.com>
2025-04-22 21:25:03 -0300

    atualizacao

---
+++ python/python_guanabara/ex079_lista_cadastro.py
@@ -0,0 +1,14 @@
+lista = []
+numero = 0
+while True:
+    numero = int(input('Digite um valor: '))
+    if numero in lista:
+        print('Valor duplicado! Não vou adicionar...')
+    else:
+        lista.append(numero)
+        print('Valor adicionado com sucesso...')
+    continuar = input('Deseja continuar? [S/N] ')
+    if continuar == 'n':
+        break
+
+print(f'Você adicionou os valores {sorted(lista)}')
